7584864f48d240cb15a1cfcb2f9cc2c4296194c9
kashifwebgit5 <gmdcf797@gmail.com>
2024-10-21 18:15:42 +0500

    feat:learning_laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>Routing</h1>
+        <a href="/home">Home Page</a>
+        <br>
+        <br>
+        <a href="/about">about Page</a>
+        <br>
+        <br>
+        <a href="/name">Users Page</a>
+    </div>
+</body>
+</html>
