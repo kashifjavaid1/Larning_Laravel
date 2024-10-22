6c3c5887acfbfce66b4902d06c6217cad6286095
kashifwebgit5 <gmdcf797@gmail.com>
2024-10-22 17:40:34 +0500

    feat:complete-form

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class RegisterController extends Controller
 {
     function registerUser(Request $req){
-        return $req;
+        // this $req all input field data get
+        // return $req;
+        echo $req->userName;
+        echo '<br/>';
+        echo $req->userEmail;
+        echo '<br/>';
+        echo $req->userCity;
     }
 }
