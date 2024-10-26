b26b8875427422e52a9cbb97d3d74d38c52b588b
kashifwebgit5 <gmdcf797@gmail.com>
2024-10-26 11:41:49 +0500

    feat:api-implement

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class StudentController extends Controller
 {
     function getStu(){
-        $students=\App\Models\Student::all();
-        return view("database.student",["std"=>$students]);
+         // $students=\App\Models\Student::all();
+        // return view("database.student",["std"=>$students]);
+        $response=Http::get('https://fakestoreapi.com/users');
+        // $response=json_decode($response->body(), true);;
+        return view("database.student", ["std" => json_decode($response)]);
+       
     }
 }
--- resources/views/database/student.blade.php
+++ resources/views/database/student.blade.php
@@ -3,12 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Student List</title>
 </head>
 <body>
     <h1>Student List</h1>
+
+    <!-- Object ke form mein JSON data access karna -->
     @foreach($std as $student)
-    <h1>{{$student->name}}</h1>
+        <h2>{{ $student->name->firstname }} {{ $student->name->lastname }}</h2>
+        <p><strong>Email:</strong> {{ $student->email }}</p>
+        <p><strong>Username:</strong> {{ $student->username }}</p>
+        <p><strong>Address:</strong> {{ $student->address->city }}, {{ $student->address->street }}, {{ $student->address->zipcode }}</p>
+        <hr>
     @endforeach
 </body>
 </html>
